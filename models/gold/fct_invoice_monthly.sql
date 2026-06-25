f9d9da4719a033783ac232e6146df49134ccf3c4
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2026-06-25 23:22:40 +0200

    fix: cast invoice_month to date (date_trunc yields timestamp_ns, rejected by Iceberg v2)

--- models/gold/fct_invoice_monthly.sql
+++ models/gold/fct_invoice_monthly.sql
@@ -1,7 +1,7 @@
 select
     customer_id,
-    date_trunc('month', reading_date)  as invoice_month,
+    cast(date_trunc('month', reading_date) as date)  as invoice_month,
     sum(kwh)                           as kwh_billed,
     sum(cost_pence) / 100.0            as amount_pounds
 from {{ ref('int_rated_intervals') }}
-group by customer_id, date_trunc('month', reading_date)
+group by customer_id, cast(date_trunc('month', reading_date) as date)
