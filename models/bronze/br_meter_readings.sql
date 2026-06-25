da4167f9bd6c1114554930496a354e47419b5538
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2026-06-25 23:15:29 +0200

    fix: cast meter_readings timestamps to timestamp(6) (Iceberg v2 rejects timestamp_ns)

--- models/bronze/br_meter_readings.sql
+++ models/bronze/br_meter_readings.sql
@@ -1,8 +1,8 @@
 select
     cast(lclid as varchar)            as lclid,
     cast(std_or_tou as varchar)       as std_or_tou,
-    cast(reading_ts as timestamp)     as reading_ts,
-    cast(reading_ts_utc as timestamp) as reading_ts_utc,
+    cast(reading_ts as timestamp(6))     as reading_ts,
+    cast(reading_ts_utc as timestamp(6)) as reading_ts_utc,
     cast(kwh as double)               as kwh,
     cast(acorn as varchar)            as acorn
 from {{ raw_parquet('meter_readings') }}
